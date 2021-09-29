1130053ae7f7a09814b4098336ce2ba94b45c559
PedroAnjosFerreira <darkplayjr@gmail.com>
2021-09-29 16:49:49 -0300

    primeiro upload

---
+++ www/js/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCo0duDrnCplrMSCOwkkbs-ljj2bBNhSvs",
+  authDomain: "cordova-app-etec2.firebaseapp.com",
+  projectId: "cordova-app-etec2",
+  storageBucket: "cordova-app-etec2.appspot.com",
+  messagingSenderId: "138424565869",
+  appId: "1:138424565869:web:d96b5965c9f773e0823f64",
+  measurementId: "G-KV466WXXHS"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
